f8cf90acff964fe7b06ffcaae967c04968a0f6f8
Gabriel Jauregui <glj.a0201@gmail.com>
2017-04-12 21:00:43 -0700

    Plugin extracted from main repository

---
+++ src/Preview/index.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Loading from '../components/Loading'
+import Preload from '../components/Preload'
+import KeyboardNav from '../components/KeyboardNav'
+import KeyboardNavItem from '../components/KeyboardNavItem'
+import getSuggestions from '../getSuggestions'
+import styles from './styles.css'
+
+const isStarWars = (query) => {
+  const str = query.toLowerCase()
+  return ['star wars', 'звёздные войны', 'звездные войны'].some(x => str.includes(x))
+}
+
+class Preview extends Component {
+  renderSuggestions(suggestions, searchFn) {
+    const className = [
+      styles.wrapper,
+      isStarWars(this.props.query) ? styles.starwars : ''
+    ].join(' ')
+    return (
+      <div className={className}>
+        <KeyboardNav>
+          <ul className={styles.list}>
+            {
+              suggestions.map(s => (
+                <KeyboardNavItem
+                  key={s}
+                  tagName={'li'}
+                  onSelect={() => searchFn(s)}
+                >
+                  {s}
+                </KeyboardNavItem>
+              ))
+            }
+          </ul>
+        </KeyboardNav>
+      </div>
+    )
+  }
+  render() {
+    const { query, search } = this.props
+    return (
+      <Preload promise={getSuggestions(query)} loader={<Loading />}>
+        {(suggestions) => this.renderSuggestions(suggestions || [], search)}
+      </Preload>
+    )
+  }
+}
+
+Preview.propTypes = {
+  query: PropTypes.string.isRequired,
+  search: PropTypes.func.isRequired
+}
+
+export default Preview
